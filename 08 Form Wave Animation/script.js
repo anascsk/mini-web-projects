f9de6c072a0b690beeec609168939a8d228f3638
anascsk <anascsk@gmail.com>
2021-07-15 22:02:26 +0530

    update

--- 08 Form Wave Animation/script.js
+++ 08 Form Wave Animation/script.js
@@ -3,6 +3,6 @@ const labels = document.querySelectorAll('.form-control label')
 labels.forEach(label => {
     label.innerHTML = label.innerText
     .split('')
-    .map((lettter, idx) => `<span style="transition-delay:${idx * 50}ms">${lettter}</span>`)
+    .map((lettter, idx) => `<span style="transition-delay:${idx * 60}ms">${lettter}</span>`)
     .join('')
 })
