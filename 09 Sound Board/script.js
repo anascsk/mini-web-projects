aa8a2e0565e26ec4f34cbe4a3414a9ed4d17ea97
anascsk <anascsk@gmail.com>
2021-07-22 22:33:24 +0530

    js updated

--- 09 Sound Board/script.js
+++ 09 Sound Board/script.js
@@ -3,4 +3,6 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
+
+    btn.innerText = sound
 })
