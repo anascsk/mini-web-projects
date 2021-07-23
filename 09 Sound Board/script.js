9beec749cdd92cceda745c47ad4dc34560648b2e
anascsk <anascsk@gmail.com>
2021-07-23 23:20:06 +0530

    updated

--- 09 Sound Board/script.js
+++ 09 Sound Board/script.js
@@ -17,5 +17,8 @@ sounds.forEach(sound => {
 function stopSongs() {
     sounds.forEach(sound => {
         const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
     })
 }
