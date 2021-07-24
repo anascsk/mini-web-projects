4294366deb51ffd23907c4a0865ad7779172ce23
anascsk <anascsk@gmail.com>
2021-07-24 22:34:47 +0530

    bugs are fixed completely

--- 09 Sound Board/script.js
+++ 09 Sound Board/script.js
@@ -7,7 +7,7 @@ sounds.forEach(sound => {
     btn.innerText = sound
     btn.addEventListener('click', () => {
         stopSongs()
-        docoument.getElementById(sound).play()
+        document.getElementById(sound).play()
     })
 
     document.getElementById('buttons').appendChild(btn)
