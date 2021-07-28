7212cfa06c059fd0273df92f5f39f9e29daca25e
anascsk <anascsk@gmail.com>
2021-07-28 22:01:42 +0530

    js update

--- 11 Event KeyCodes/script.js
+++ 11 Event KeyCodes/script.js
@@ -1,2 +1,10 @@
 const insert = document.getElementById('insert')
 
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML = `
+    <div class="key">
+    ${event.key === ' ' ? 'Space' : event.key} 
+    <small>event.key</small>
+  </div>
+
+})
