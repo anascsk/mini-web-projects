eb365d1a8d1181e2f6fc30153f5b252a2f1174a5
anascsk <anascsk@gmail.com>
2021-08-06 22:11:35 +0530

    js

---
+++ 17 Background Slider/script.js
@@ -0,0 +1,17 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+  activeSlide++
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0
+  }
+
+  setBgToBody()
+  setActiveSlide()
+})
