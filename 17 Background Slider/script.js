a4bb7cd7560eb170e055537e255a1b3c6ddddcf4
anascsk <anascsk@gmail.com>
2021-08-06 22:18:58 +0530

    js finished

--- 17 Background Slider/script.js
+++ 17 Background Slider/script.js
@@ -15,3 +15,27 @@ rightBtn.addEventListener('click', () => {
   setBgToBody()
   setActiveSlide()
 })
+
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+  
+    if (activeSlide < 0) {
+      activeSlide = slides.length - 1
+    }
+  
+    setBgToBody()
+    setActiveSlide()
+  })
+  
+  setBgToBody()
+  
+  function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+  }
+  
+  function setActiveSlide() {
+    slides.forEach((slide) => slide.classList.remove('active'))
+  
+    slides[activeSlide].classList.add('active')
+  }
