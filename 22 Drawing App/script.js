5fe5c8689a92060b1d7ddce4db590f5dd0ab5ad6
anascsk <anascsk@gmail.com>
2021-08-09 23:12:07 +0530

    css

---
+++ 22 Drawing App/script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
+const ctx = canvas.getContext('2d');
